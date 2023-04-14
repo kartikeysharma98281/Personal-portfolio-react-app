29d93f7e31f11276e895a4aa5fa72e1ed5bfc817
kartikey sharma <73027263+kartikeysharma98281@users.noreply.github.com>
2023-04-14 18:07:23 +0000

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "Kartikey Sharma",
+  title: "MERN Stack Developer",
+  email: "kartikeysharma.sharma@gmail.com",
+  gitHub: "https://github.com/kartikeysharma98281",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "https://www.linkedin.com/in/kartikey-sharma-879747220/",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
